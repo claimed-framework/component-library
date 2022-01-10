0cb0d993b9a52bb30fdeeae7213c66b8a3114cc5
Romeo Kienzler <romeo.kienzler@ch.ibm.com>
2022-01-10 16:02:51 +0100

    backup

--- covid19.sql
+++ covid19.sql
@@ -1,7 +1,16 @@
 drop view if exists cube cascade;
 
 create or replace view destatis6_view as 
-    SELECT destatis6.zeit as year, replace(destatis6."3_auspraegung_code", 'MONAT'::text, ''::text)::int as month, to_date(concat(destatis6.zeit, replace(destatis6."3_auspraegung_code", 'MONAT'::text, ''::text)), '%YYYYMM'::text) AS "time",     (replace(destatis6.bev002__gestorbene__anzahl, '...'::text, '0'::text))::integer AS value    FROM destatis6;
+    SELECT destatis6.zeit as year,
+           replace(destatis6."3_auspraegung_code", 'MONAT', '')::int as month, 
+           to_date(concat(destatis6.zeit, replace(destatis6."3_auspraegung_code", 'MONAT', '')), '%YYYYMM'::text) AS "time",
+           sum((replace(destatis6.bev002__gestorbene__anzahl, '...', '0'))::int) AS value
+           --replace(destatis6.bev002__gestorbene__anzahl, '...', '0')::int AS value
+    FROM destatis6 
+    group by year, month, time 
+    order by time asc;
+    
+   
 
 create or replace view destatis6_moments as 
     SELECT month, min(value) as minimum, avg(value)::int as average, max(value) as maximum from destatis6_view where year > 2000 and year < 2021 group by month ; 
@@ -20,5 +29,11 @@ create table facts as SELECT * FROM generate_series('1950-01-01'::timestamp, '20
 create or replace view cube as select f.*, m.minimum, m.maximum, m.average, v.value as deaths, rki.value as covid_deaths from facts f 
     left outer join destatis6_moments_fused m on f.time=m.time
     left outer join destatis6_view v on f.time=v.time
-    left outer join rki_covid_deaths_view rki on f.time=rki.timestamp
+    left outer join rki_covid_deaths_view rki on f.time=rki.timestamp;
 
+create or replace view cube_view as
+/*
+    select time, COALESCE(minimum, 0 ) as minimum, COALESCE(maximum, 0 ) as maximum, COALESCE(average, 0) as average, COALESCE(deaths, 0) as deaths, COALESCE(covid_deaths, 0 ) as covid_deaths from (select distinct * from cube order by time asc) as t*/
+    select distinct * from cube order by time asc
+    
+    ;
