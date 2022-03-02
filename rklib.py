b595c25a752874602054443fb8785611c5e863e4
Merey Balgabayev <mbalgabayev@Mereys-MacBook-Pro.local>
2022-03-03 00:49:12 +0600

    rklib edit

--- rklib.py
+++ rklib.py
@@ -26,19 +26,7 @@ def submit(submitterEmail,secret,key,submission_part, all_parts, data):
             print (response.text)
             print ("-------------------------")
             
-def submitAll(submitterEmail,secret,key,parts_and_data):
-        submission = {
-                    "assignmentKey": key,
-                    "submitterEmail":  submitterEmail,
-                    "secret":  secret,
-                    "parts": {}
-                  }
-        for part, output in parts_and_data.items():
-            if output is not None:
-                submission["parts"][part] = {"output": output}
-            else:
-                submission["parts"][part] = dict()
-        response = requests.post('https://www.coursera.org/api/onDemandProgrammingScriptSubmissions.v1', data=json.dumps(submission))
+
         if response.status_code == 201:
             print ("Submission successful, please check on the coursera grader page for the status")
             print ("-------------------------")
