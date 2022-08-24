f8e1c0181f5b10dd1fbdda4df4a39ba1845be25b
Romeo <romeo.kienzler@ch.ibm.com>
2022-08-24 12:40:02 +0000

    add auto build for container images

---
+++ component-library/anomaly/anomaly-score-unsupervised/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+jupyter nbconvert --to script anomaly-score-unsupervised.ipynb    
+docker build -t claimed-anomaly-score-unsupervised:`echo $version` -f anomaly-score-unsupervised/Dockerfile .
+docker tag claimed-anomaly-score-unsupervised:`echo $version` `echo $repository`/claimed-anomaly-score-unsupervised:`echo $version`
+docker push `echo $repository`/claimed-anomaly-score-unsupervised:`echo $version`
