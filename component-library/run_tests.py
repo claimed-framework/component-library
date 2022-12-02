19f8762e511c44b8d820fc3466f475465e7397ef
Romeo Kienzler <romeo.kienzler@protonmail.com>
2022-12-02 09:27:54 +0100

    fix path

--- component-library/run_tests.py
+++ component-library/run_tests.py
@@ -5,7 +5,7 @@ import subprocess
 class TestClaimed(unittest.TestCase):
 
     def test_ibm_sql_query_cpd_test(self):
-        correct = subprocess.run(['ipython', './component-library/transform/ibm-sql-query-cpd-test.ipynb'], check=True, text=True)
+        correct = subprocess.run(['ipython', './transform/ibm-sql-query-cpd-test.ipynb'], check=True, text=True)
         self.assertFalse('Foo'.isupper())
 
 unittest.main()
